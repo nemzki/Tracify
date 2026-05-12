cc64634dd1afe2173191cb01c7f7df8e76828df2
nemzki <maglawaymark017@gmail.com>
2026-05-12 09:01:04 +0800

    Initialize login manager

--- extensions.py
+++ extensions.py
@@ -1,4 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # INITIALIZE DB
 db = SQLAlchemy()
+
+# INITIALIZE LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Please sign in to access this page.'
+login_manager.login_message_category = 'error'
